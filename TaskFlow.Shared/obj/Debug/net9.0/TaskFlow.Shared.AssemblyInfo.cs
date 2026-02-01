d4eb6e9039c140bc3590d0584f848a1024acb154
Lalindu Palliyaguruge <151996159+lalindumalaka@users.noreply.github.com>
2026-02-01 09:17:49 +0530

    changes

--- TaskFlow.Shared/obj/Debug/net9.0/TaskFlow.Shared.AssemblyInfo.cs
+++ TaskFlow.Shared/obj/Debug/net9.0/TaskFlow.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e61ea9a149bab96608bcb5bef2f7a23035d5912")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
